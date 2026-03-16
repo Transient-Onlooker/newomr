b0b89855deb49f78c6f1e27d84f8662c515d0553
Transient Onlooker <swapomit@gmail.com>
2026-03-16 12:58:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/newomr/", // 👈 이 줄을 반드시 추가해주세요! (저장소 이름)
 })
